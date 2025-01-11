65a81da291a63510e80dc37044475cc9a6911a01
Swarnendu <swarnendumaity633@gmail.com>
2025-01-11 05:32:53 +0530

    chore: register crypto routes in the application

--- src/routes/price.route.ts
+++ src/routes/price.route.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import { getCryptoPrices } from "../controller/price";
+import { getCryptoStats } from "../controller/stats";
 
 const router = express.Router();
 
+//Task 1
 router.get("/crypto-prices", getCryptoPrices);
+
+//Task 2
+router.get("/stats", getCryptoStats);
+
 
 export default router;
